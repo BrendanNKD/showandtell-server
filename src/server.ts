bb2d89fe9717dce193bf91bb66ed2d1f31d480b0
ahbren <ahbren@outlook.com>
2023-11-09 01:24:26 +0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -16,16 +16,16 @@ const server = express();
 
 const cookieParser = require("cookie-parser");
 
+server.use(cookieParser());
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
 server.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
   })
 );
-
-server.use(cookieParser());
-server.use(bodyParser.json({ limit: "50mb" }));
-server.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 server.use(express.json());
 
