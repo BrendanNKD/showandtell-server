b1d941dff84cea97c9d352f38c0ca9a9d0a716bf
ahbren <ahbren@outlook.com>
2023-11-09 06:12:59 +0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -16,16 +16,16 @@ const server = express();
 
 const cookieParser = require("cookie-parser");
 
+server.use(cookieParser());
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
 server.use(
   cors({
     origin: "https://whateyesee.netlify.app",
     credentials: true,
   })
 );
-
-server.use(cookieParser());
-server.use(bodyParser.json({ limit: "50mb" }));
-server.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 server.use(express.json());
 
